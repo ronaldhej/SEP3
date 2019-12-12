6c508975607fc52d9471ec6f71681681109188dc
Altair97 <przemyslaw.reg@gmail.com>
2019-12-12 16:16:26 +0100

    test

---
+++ src/module-info.java
@@ -0,0 +1,28 @@
+module SEP_3 {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+
+    // git ignore test
+
+    exports main to javafx.graphics;
+    exports view.login to javafx.fxml;
+    exports view.user.mainUser to javafx.fxml;
+    exports view.user.dashBoard to javafx.fxml;
+    exports view.user.burndown to javafx.fxml;
+    exports view.user.report to javafx.fxml;
+    exports view.user.team to javafx.fxml;
+    exports view.user.createTeam to javafx.fxml;
+    exports view.user.task to javafx.fxml;
+    exports Model to java.base;
+
+
+    opens view.login to javafx.fxml;
+    opens view.user.dashBoard to javafx.fxml;
+    opens view.user.mainUser to javafx.fxml;
+    opens view.user.burndown to javafx.fxml;
+    opens view.user.report to javafx.fxml;
+    opens view.user.team to javafx.fxml;
+    opens view.user.createTeam to javafx.fxml;
+    opens view.user.task to javafx.fxml;
+}
